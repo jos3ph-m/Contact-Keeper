8f491200c888451cae2b2def8279569e44bc23e0
Joseph M <jmedina1985@hotmail.com>
2020-06-26 23:55:47 -0700

    Connect MongoDB to app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Entry point to backend
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // add route
 app.get("/", (req, res) => {
