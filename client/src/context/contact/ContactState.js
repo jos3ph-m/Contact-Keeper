78276467fe6d5c0436ba8ebd31c1b86a1db3336d
Joseph M <jmedina1985@hotmail.com>
2020-07-01 23:57:01 -0700

    Delete contact from UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,6 +48,9 @@ const ContactState = (props) => {
   };
 
   // Delete Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // Set Current Contact
 
