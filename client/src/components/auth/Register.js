642cc2b7ee68047d138426983424d675e4fdae53
Joseph M <jmedina1985@hotmail.com>
2020-07-05 23:00:50 -0700

    Added register to app

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { set } from "mongoose";
 
 const Register = () => {
   const [user, setUser] = useState({
@@ -10,12 +11,19 @@ const Register = () => {
 
   const { name, email, password, password2 } = user;
 
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Register Submit");
+  };
+
   return (
     <div className="form-container">
       <h1>
         Account <span className="text-primary">Register</span>
       </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
           <input type="text" name="name" value={name} onChange={onChange} />
