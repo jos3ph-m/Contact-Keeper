377e12ee8b758fc6ca02e0e19e8b076cfed8e5f0
Joseph M <jmedina1985@hotmail.com>
2020-07-08 13:40:17 -0700

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Entry point to backend
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -16,14 +17,24 @@ app.use(
 );
 
 // add route
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the ContactKeeper API" });
-});
+// app.get("/", (req, res) => {
+
+// });
 
 // Define Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/users", require("./routes/users"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
